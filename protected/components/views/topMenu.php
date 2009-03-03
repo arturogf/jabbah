9a22fd4fe823ce4f8b162d2a4bfbd75071dd01c0
Arturo Gonzalez <arturogf@gmail.com>
2009-03-03 17:54:18 +0100

    reset ecarules repository

--- protected/components/views/topMenu.php
+++
@@ -1,6 +0,0 @@
-<ul>
-<?php foreach($items as $item): ?>
-<li><?php echo CHtml::link($item['label'],$item['url'],
-	$item['TopActive'] ? array('class'=>'TopActive') : array()); ?></li>
-<?php endforeach; ?>
-</ul>

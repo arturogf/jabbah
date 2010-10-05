ee5d141920a0e641fd683e06d8ed4fa2c24bdff6
arturogf <arturogf@arturogf-laptop.(none)>
2010-10-05 13:05:51 +0200

    - add the AFL 3.0 license comment at the beginning of each file.

--- JABBAH/src/jabbah/Transition.java
+++ JABBAH/src/jabbah/Transition.java
@@ -1,3 +1,25 @@
+/* 
+This file is part of the Jabbah Framework, a software that is able to
+carry out a transformation between Business Process Models into 
+Hierarchical Task Networks Planning domains.
+
+    Copyright (C) 2010
+    	      Arturo Gonzalez-Ferrer, arturogf@decsai.ugr.es
+
+    Este programa es software libre: usted puede redistribuirlo y/o modificarlo 
+    bajo los términos de la Academic Free License ("AFL") v. 3.0).
+
+    Este programa se distribuye con la esperanza de que sea útil, pero 
+    SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita 
+    MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO. 
+    Consulte los detalles de la Academic Free License ("AFL) v 3.0 para obtener 
+    una información más detallada. 
+
+    Debería haber recibido una copia de la Academic Free License ("AFL") v. 3.0
+    junto a este programa. 
+
+
+*/
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -23,3 +45,4 @@ public class Transition {
     {
     }
 }
+
